4ca9530689329d2236b2d6d23588b13489da96d7
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-06-26 14:54:48 +0530

    some changes

--- UIAnimationTransition/ViewController.swift
+++ UIAnimationTransition/ViewController.swift
@@ -29,25 +29,25 @@ class ViewController: UIViewController {
       let originalCenter = planeImage.center
         UIView.animateKeyframes(withDuration: 5, delay: 0.0, options: [.repeat],
         animations: {
-          UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25,
+        UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25,
       animations: {
         self.planeImage.center.x += 80.0
         self.planeImage.center.y -= 10.0
       })
-          UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.4) {
+        UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.4) {
             self.planeImage.transform = CGAffineTransform(rotationAngle: -.pi / 8)
           }
           
-          UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
-              self.planeImage.center.x += 100.0
+        UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
+              self.planeImage.center.x += 200.0
               self.planeImage.center.y -= 50.0
               self.planeImage.alpha = 0.0
           }
-            UIView.addKeyframe(withRelativeStartTime: 0.51, relativeDuration: 0.01) {
+        UIView.addKeyframe(withRelativeStartTime: 0.51, relativeDuration: 0.01) {
               self.planeImage.transform = .identity
               self.planeImage.center = CGPoint(x: 0.0, y: originalCenter.y)
             }
-            UIView.addKeyframe(withRelativeStartTime: 0.55, relativeDuration: 0.45) {
+        UIView.addKeyframe(withRelativeStartTime: 0.55, relativeDuration: 0.45) {
               self.planeImage.alpha = 1.0
               self.planeImage.center = originalCenter
             }
