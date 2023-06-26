8dab859e1284c62ccf3fdaf3198ed2a5c2f2f309
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-06-26 11:56:56 +0530

    added animation

--- UIAnimationTransition/ViewController.swift
+++ UIAnimationTransition/ViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var animationView: UIView!
+    var newView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        animationView = UIView(frame: view.bounds)
+        animationView.frame = view.bounds
+        view.addSubview(animationView)
+        newView = UIImageView(image: UIImage(named: "sky")!)
+        newView.center = animationView.center
+          self.animationView.addSubview(newView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+      //create new view
+    
+      //add the new view via transition
+      UIView.transition(with: animationView,
+        duration: 5,
+        options: [.curveEaseOut, .transitionFlipFromRight],
+        animations: {
+          self.newView.removeFromSuperview()
+        },
+        completion: nil
+      )
     }
 
 
